f6056662585e6e63b39c992523a80bf0ad57b226
OGMyst <hughwiliampadmore@hotmail.co.uk>
2020-05-12 11:37:38 +0000

    Added five new tests

--- Jasmine/specs/calcSpec.js
+++ Jasmine/specs/calcSpec.js
@@ -22,7 +22,7 @@ describe("randomiser", function(){
         expect(result).toBe(true);
     });
 
-    it("should return true", function(){
+    it("should return 3", function(){
         // arrange
         let cardname = cardValue[2];
         let indexnum = cardname;
@@ -32,4 +32,61 @@ describe("randomiser", function(){
         expect(result).toBe("3");
     });
 
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,12));
+        let cardname = cardValue[i];
+        let indexnum = isNaN(cardname);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+
+    it("should return Hearts", function(){
+        // arrange
+        let cardname = cardSuit[0];
+        let indexnum = cardname;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe("Hearts");
+    });
+
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,3));
+        let cardname = cardSuit[i];
+        let indexnum = isNaN(cardname);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+
+    it("should return AofHearts ", function(){
+        // arrange
+        let cardType = cardSuit[0];
+        let cardname = cardValue[0];
+        let indexnum = cardname + "of"+ cardType;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe("AofHearts");
+    });
+
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,3));
+        let cardType = cardSuit[i];
+
+        let j = Math.floor(Math.random(0,12));
+        let cardname = cardValue[j];
+
+        let indexnum = isNaN (cardname + "of"+ cardType);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
 });
